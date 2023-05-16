5992ccd613b890977c367456294f9a96c7a00787
danicakirana <100155013+danicakirana@users.noreply.github.com>
2023-05-16 22:09:12 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:datafilm_omdb/view/page_search_film.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OMDB FILM',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.light().copyWith(primary: Colors.tealAccent),
+      ),
+      home: PageSearchFilm(),
+    );
+  }
+}
---
+++ lib/view/page_list_film.dart
@@ -0,0 +1,92 @@
+import 'package:datafilm_omdb/view/page_detail_film.dart';
+
+import '../controller/films_data_source.dart';
+import '../model/films.dart';
+import 'package:flutter/material.dart';
+
+class PageListFilm extends StatefulWidget {
+  final String text;
+  const PageListFilm({Key? key, required this.text}) : super(key: key);
+  @override
+  State<PageListFilm> createState() => _PageListFilmState();
+}
+
+class _PageListFilmState extends State<PageListFilm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List Film"),
+      ),
+      body: _buildListUsersBody(),
+    );
+  }
+
+  Widget _buildListUsersBody() {
+    return Container(
+      child: FutureBuilder(
+        future: FilmsDataSource.instance.loadFilms(widget.text),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasError) {
+            // Jika data ada error maka akan ditampilkan hasil error
+            return _buildErrorSection();
+          }
+          if (snapshot.hasData) {
+            // Jika data ada dan berhasil maka akan ditampilkan hasil datanya
+            FilmList filmsModel = FilmList.fromJson(snapshot.data);
+            return _buildSuccessSection(filmsModel);
+          }
+          return _buildLoadingSection();
+        },
+      ),
+    );
+  }
+
+  Widget _buildErrorSection() {
+    return Text("Error");
+  }
+
+  Widget _buildLoadingSection() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _buildSuccessSection(FilmList data) {
+    return ListView.builder(
+      itemCount: data.search!.length,
+      itemBuilder: (BuildContext context, int index) {
+        return _buildItemUsers(data.search![index]);
+      },
+    );
+  }
+
+  Widget _buildItemUsers(Search filmData) {
+    return InkWell(
+      onTap: () => Navigator.push(context,
+          MaterialPageRoute(builder: (context) => PageDetailFilm(text: filmData.imdbID!))
+      ),
+      child: Card(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: 100,
+              child: Image.network(filmData.poster!),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(filmData.title!),
+                Text(filmData.year!)
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
